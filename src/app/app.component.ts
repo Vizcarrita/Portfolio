58b8668eec6ae55498b76addab414687ddf57f4c
Mattias Vizcarra <m.vizcarra17@hotmail.com>
2024-04-19 12:04:06 -0400

    feat(heroComponent)Implementacion del hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './pages/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
